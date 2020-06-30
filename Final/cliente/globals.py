293af8bfb399b4fbba7c2657c97185ac9615c34f
usac201114651 <2096434980412@ingenieria.usac.edu.gt>
2020-06-29 20:10:02 -0600

    Version final
    Versionfinal

---
+++ Final/cliente/globals.py
@@ -0,0 +1,25 @@
+#CATC variables
+ALIVE_PERIOD            = 2            #CATC Período entre envío de tramas ALIVE
+ALIVE_CONTINUOUS        = 0.1      #CATC Período entre envío de tramas ALIVE si no hay respuesta
+
+#CATC COMMANDS
+COMMAND_FRR             = b'\x02'
+COMMAND_FTR             = b'\x03'
+COMMAND_ALIVE           = b'\x04'
+COMMAND_ACK             = b'\x05'
+COMMAND_OK              = b'\x06'
+COMMAND_NO              = b'\x07'
+
+#CATC System filenames
+USERS_FILENAME          = "usuario.txt"
+ROOMS_FILENAME          = "salas.txt"
+
+#CATC System ident b
+USER_ID_2                 = "201114651"
+USER_ID_3                 = "201701038"
+USER_ID_1                 = "201700512"
+
+
+
+
+
